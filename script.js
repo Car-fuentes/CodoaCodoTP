95dc69508047c015493611b6405015f15b2cb9fa
Car-fuentes <carolinainesfuentes@gmail.com>
2023-06-11 16:12:17 -0300

    armando el script

--- script.js
+++ script.js
@@ -1,151 +1,57 @@
-const formulario = document.getElementById("formulario");
-var nombre = document.getElementById("nombre");
-var apellido = document.getElementById("apellido");
-var email = document.getElementById("email");
-var cantidad = document.getElementById("cantidad");
-var categoria = document.getElementById("categoria").value;
 
-
-
-var categ = categoria;
-    console.log(categ);
-
-/*var valorTicket = 200;
-
-    if (categ == 1){
-    alert("estudiante");
-    function tktEstuadiante(){
-        var valorTicket =200
-        function descuento80(){
-        valorTicket =0.20
-        console.log(valorTicket)
-        }
-    return descuento80;
-    }} 
-*/
-    var ticket ={
-        precio: 200
-    }
-
-    var descuento = function(a){
-        return this.precio * a /100;
-    }
-
-    var tktEstudiante = descuento.call(ticket, 20)
-    console.log(tktEstudiante)
-
-    var tktGeneral = descuento.call(ticket, 100)
-    console.log(tktGeneral)
-
-    var tktTrainee = descuento.call(ticket, 50)
-    console.log(tktTrainee)
-
-    var tktJunior = descuento.call(ticket, 85)
-    console.log(tktJunior)
-
-    /*function tktEstudiante(){
-        var valorTicket = 200
-        function descuento80(){
-        valorTicket *= 0.20 
-        console.log(valorTicket)   
-        }
-        return descuento80;
-    }*/
-
- /*  var precioEstudiante = tktEstudiante();
-    precioEstudiante();
-
-var valorTicket = 200
-
-function tktGeneral(){
-    function sinDescuento(){
-    valorTicket = valorTicket 
-    console.log(valorTicket)
-    }
-    return sinDescuento;
-}
-var precioGeneral = tktGeneral();
-precioGeneral();
-
-    function tktEstudiante(){
-        var valorTicket = 200
-        function descuento80(){
-        valorTicket *= 0.20 
-        console.log(valorTicket)   
-        }
-        return descuento80;
-    }
-
-    var precioEstudiante = tktEstudiante();
-    precioEstudiante();
-
-
-
-
-
-
-/*if (categoria.value == 1){
-    console.log("estudiante");
-    function haceFuncion(){
-        var valorTicket = 200;
-        console.log(valorTicket);
-        function haceCuenta(){
-        valorTicket*200/100
-        }
-    return haceCuenta;
-    }}
-
-
-/*if (categoria.value == 1){
-    console.log("estudiante");
-    function haceFuncion(){
-        var valorTicket = 200;
-        console.log(valorTicket);
-        function haceCuenta(){
-        valorTicket*200/100
-        }
-    return haceCuenta;
-    }}
-
-var miCuenta = haceCuenta();
-miCuenta();*/
-
-
+let formulario = document.getElementById("formulario");
+let nombre = document.getElementById("nombre");
+let apellido = document.getElementById("apellido");
+let email = document.getElementById("email");
+let cantidad = document.getElementById("cantidad");
+let categoria = document.getElementById("categoria");
+let resumen = document.getElementById("resumen"); 
 
 formulario.addEventListener("submit", (x)=>{
     x.preventDefault();
     console.log("funciona");
 
-
-    console.log(apellido.value);
     console.log(nombre.value);
+    console.log(apellido.value);
     console.log(email.value);
     console.log(cantidad.value);
-    console.log(categoria);
+    console.log(categoria.value);
 
-    console.log(typeof apellido.value);
-    console.log(typeof nombre.value);
-    console.log(typeof email.value);
-    console.log(typeof cantidad.value);
-    console.log(typeof categoria);
-    console.log(typeof +categoria);
+    let ticket ={
+        precio: 200
+    }
+
+    let descuento = function(a){
+        return this.precio * a /100;
+    }
+
+    let tktEstudiante = descuento.call(ticket, 20)
+    console.log(tktEstudiante)
+
+    let tktGeneral = descuento.call(ticket, 100)
+    console.log(tktGeneral)
+
+    let tktTrainee = descuento.call(ticket, 50)
+    console.log(tktTrainee)
+
+    let tktJunior = descuento.call(ticket, 85)
+    console.log(tktJunior)
+
+    switch (categoria.value){
+        case "estudiante":
+            total = parseInt(cantidad.value)*tktEstudiante;
+            break;
+            case "trainee":
+                total = parseInt(cantidad.value)*tktTrainee;
+                break;
+            case "junior":
+                total = parseInt(cantidad.value)*tktJunior;
+                break;
+            case "general":
+                total = parseInt(cantidad.value)*tktGeneral;
+                break;
+    }
+    
+        resumen.innerHTML = `El total de tu compra es: ${total}`;
+
 })
-
-function suma(cant, cat){
-    console.log(1)
-    let sumando = cant + cat
-    return sumando
-}
-
-        
-
-
-
-function suma(cantidad, miCuenta){
-    let cant = cantidad
-    console.log(cant)
-    let cuenta = miCuenta
-    console.log(cuenta)
-    let sumando = cant + cuenta
-    return sumando
-}
